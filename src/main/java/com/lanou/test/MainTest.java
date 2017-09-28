e0394639fabc872db0704df003381cee18296a61
dllo <1084372318@qq.com>
2017-09-28 15:13:43 +0800

    第一次提交，添加工程文件

---
+++ src/main/java/com/lanou/test/MainTest.java
@@ -0,0 +1,13 @@
+package com.lanou.test;
+
+/**
+ * Created by dllo on 17/9/28.
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("helloMaven");
+    }
+
+}
